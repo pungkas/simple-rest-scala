f8242ba744efe586cd3d6e29e81983758bdad2f7
Pungkas Yoga Kesuma <yogapunya@yahoo.com>
2015-10-21 18:56:00 +0700

    Learn to commit change

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,13 +3,32 @@ package controllers
 import play.api.libs.json._
 import play.api.mvc._
 import models.Book._
+import models.Todo._
+import play.api.data.Form._
 
 object Application extends Controller {
+
+  def index = Action {
+      //Ok("test")
+      Ok(views.html.index())
+  }
 
   def listBooks = Action {
     Ok(Json.toJson(books))
   }
-
+  
+  //val todoForm: Form[Todo] = Form {
+  //    mapping(
+  //      "name"     -> text
+  //    ){Todo.apply}(Todo.unapply)
+  //}
+  
+  //def addTodo = Action { implicit request => 
+  //  val todo = todoForm.bindFromRequest.get
+  //  DB.save(todo)
+  //  Redirect(routes.Application.index())
+  //}
+  
   def saveBook = Action(BodyParsers.parse.json) { request =>
     val b = request.body.validate[Book]
     b.fold(
@@ -18,6 +37,23 @@ object Application extends Controller {
       },
       book => {
         addBook(book)
+        Ok(Json.obj("status" -> "OK"))
+      }
+    )
+  }
+  
+  def listTodos = Action {
+    Ok(Json.toJson(todos))
+  }
+
+  def saveTodo = Action(BodyParsers.parse.json) { request =>
+    val b = request.body.validate[Todo]
+    b.fold(
+      errors => {
+        BadRequest(Json.obj("status" -> "OK", "message" -> JsError.toFlatJson(errors)))
+      },
+      todo => {
+        addTodo(todo)
         Ok(Json.obj("status" -> "OK"))
       }
     )
--- build.sbt
+++ build.sbt
@@ -3,3 +3,6 @@ name := """simple-rest-scala"""
 version := "1.0-SNAPSHOT"
 
 lazy val root = project.in(file(".")).enablePlugins(PlayScala)
+
+
+fork in run := true
